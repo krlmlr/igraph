efa07513222c6321643e1ba214b84468286e21c2
Kirill Müller <kirill@cynkra.com>
2026-04-18 20:08:06 +0200

    fixup! chore: Header comments

--- examples/simple/igraph_minimal_separators.c
+++ examples/simple/igraph_minimal_separators.c
@@ -26,7 +26,10 @@
 int main(void) {
     igraph_t graph;
     igraph_vector_int_list_t separators;
-    igraph_integer_t i, n;
+    igraph_int_t i, n;
+
+    /* Initialize the library. */
+    igraph_setup();
 
     igraph_famous(&graph, "zachary");
     igraph_vector_int_list_init(&separators, 0);
